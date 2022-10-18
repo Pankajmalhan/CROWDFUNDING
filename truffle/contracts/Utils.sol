6a3f2f151b860035af1b7445b89acee3c95406a1
arorashivam <arora.shivam@tftus.com>
2022-10-18 22:41:27 +0530

    Fix rebase

--- truffle/contracts/Utils.sol
+++ truffle/contracts/Utils.sol
@@ -16,15 +16,35 @@ library LibraryEvents {
 
 library LibraryErrors {
     //Events
-    event priceError(uint256 _project_target_price, uint256 _project_minimum_fund_price);
-    event wrongDeadline(uint256 _projest_deadline_date_unix);
-
-    function checkForMinumumFund(uint32 _project_target_price, uint32 _project_minimum_fund_price) public pure{
-        require(_project_target_price > _project_minimum_fund_price, 'Target price should not be less than minimum fund price');
-    }
-    
     error SpendMoreEth();
     error NotProjectOwner();
     error NotInAwaitingFunding();
     error NotFundingComplete();
+}
+
+library HelperFunctions {
+    //Function to find string of given string
+    function strlen(string memory s) internal pure returns (uint256) {
+        uint256 len;
+        uint256 i = 0;
+        uint256 bytelength = bytes(s).length;
+        for (len = 0; i < bytelength; len++) {
+            bytes1 b = bytes(s)[i];
+            if (b < 0x80) {
+                i += 1;
+            } else if (b < 0xE0) {
+                i += 2;
+            } else if (b < 0xF0) {
+                i += 3;
+            } else if (b < 0xF8) {
+                i += 4;
+            } else if (b < 0xFC) {
+                i += 5;
+            } else {
+                i += 6;
+            }
+        }
+        return len;
+    }
+
 }
