a61be3a1f0276ed5af106bdb1f0f6b0b1a6965f1
atulsharmaTFT <sharma.atul@tftus.com>
2022-10-25 15:15:17 +0530

    Nav Bar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {  NavLink } from "react-router-dom";
+import "./navBar.css";
+
+const Navbar= () =>{
+  return (
+    <>
+    <nav className="main-nav">
+      {/* 1st logo part  */}
+      <div className="logo">
+        <h2>
+          <span>C</span>rowd
+          <span> F</span>unding
+        </h2>
+      </div>
+
+      {/* 2nd menu part  */}
+      <div
+        className="menu-link">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/AboutUs">About Us</NavLink>
+          </li>
+          <li>
+            <NavLink to="/service">services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">contact</NavLink>
+          </li>
+        </ul>
+        
+      </div>
+    </nav>
+    </>
+  );
+}
+export default Navbar;
