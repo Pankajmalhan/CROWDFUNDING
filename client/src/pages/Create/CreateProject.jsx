f804bca334042ae1eec93572d218f14adfbdce38
atulsharmaTFT <sharma.atul@tftus.com>
2022-11-02 15:43:08 +0530

    Ipfs doc upload

--- client/src/pages/Create/CreateProject.jsx
+++ client/src/pages/Create/CreateProject.jsx
@@ -3,6 +3,8 @@ import Image from "../../Assets/img/eth.png";
 import { getUserAddress } from "../../actions/Web3Actions";
 import { useForm, useWatch } from "react-hook-form";
 import { copy } from "../../helper/function";
+import { Web3Storage } from "web3.storage/dist/bundle.esm.min.js";
+
 export const CreateProject = () => {
   const [userAddress, setUserAddress] = React.useState("");
   const {
@@ -17,8 +19,9 @@ export const CreateProject = () => {
     mode: "onChange",
   });
   const data = useWatch({ control });
-  const mindate =  new Date().toLocaleDateString();
-  React.useEffect(() => {
+  const mindate = new Date().toLocaleDateString();
+  const API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGZjZDJEMjcyMGE3YjMzYzRiQkExZjEwNEMxYmIwZTVhNjA1NzVmRGMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjczMTMxNDg2OTEsIm5hbWUiOiJjcm93ZGZ1bmRpbmcifQ.wW-i0_7uqiM1Jyh7hTWfpPGrH7I18f6LS47nAZzj9Ts"
+    React.useEffect(() => {
     setAddress();
   }, [userAddress]);
 
@@ -28,11 +31,44 @@ export const CreateProject = () => {
     return address;
   }
 
+  async function handleImageUpload() {
+    try{
+      if(data?.image?.length) {
+    const client = new Web3Storage({ token: API_TOKEN });
+    const fileInput = document.querySelector('input[type="file"]');
+
+    const rootCid = await client.put(fileInput.files);
+
+    const info = await client.status(rootCid);
+    console.log(info);
+    // Fetch and verify files from web3.storage
+    const res = await client.get(rootCid);
+    const files = await res.files();
+    for (const file of files) {
+      console.log(`${file.cid} ${file.name} ${file.size}`);
+      return `${file.cid}`
+    }
+  }
+  else {
+    return 
+  }
+  }
+  catch (error) {
+    console.log(error , "Error")
+  }
+  }
   function onSubmit(e) {
     if (data) {
-      const res = { ...data, ...{ publicAddress: userAddress ?? "" } };
-      console.log(res, "data");
+      handleImageUpload().then((d)=>{
+        if(d) {
+          const res = { ...data, ...{ publicAddress: userAddress ?? "" , cid : d}};
+          alert(" Project successfully Added")
+      }
+      else {
+        const res = { ...data, ...{ publicAddress: userAddress ?? "" , cid : ""}};
       alert(" Project successfully Added")
+      }
+    })
     }
   }
   return (
@@ -46,10 +82,9 @@ export const CreateProject = () => {
               <input
                 style={{ fontSize: "1.5rem" }}
                 placeholder="Enter title"
-                {...register("title",{required: "*Title Required"})}
+                {...register("title", { required: "*Title Required" })}
                 type={"text"}
-              >
-              </input>
+              ></input>
             </div>
             {errors?.title && <p>{errors?.title.message}</p>}
             <div className="description">
@@ -57,10 +92,12 @@ export const CreateProject = () => {
               <input
                 style={{ fontSize: "1.5rem" }}
                 placeholder="Enter description"
-                {...register("description", {required: "*Description Required"})}
+                {...register("description", {
+                  required: "*Description Required",
+                })}
                 type={"textarea"}
                 className="desc"
-              ></input>    
+              ></input>
             </div>
             {errors?.description && <p>{errors?.description.message}</p>}
             <div className="deadline">
@@ -68,21 +105,26 @@ export const CreateProject = () => {
               <input
                 style={{ fontSize: "1.5rem" }}
                 placeholder="Select last date"
-                {...register("deadline" , {required: "*Deadline Required" , min: {mindate}})}
+                {...register("deadline", {
+                  required: "*Deadline Required",
+                  min: { mindate },
+                })}
                 type={"date"}
-              ></input>      
-              </div>
-              {errors?.deadline && <p>{errors?.deadline.message}</p>}         
+              ></input>
+            </div>
+            {errors?.deadline && <p>{errors?.deadline.message}</p>}
             <div className="targetprice">
               <h2>Target Price:</h2>
               <input
                 style={{ fontSize: "1.5rem" }}
                 placeholder="Select Target Amount"
-                {...register("targetPrice",{required: "*Target ammount Required"})}
+                {...register("targetPrice", {
+                  required: "*Target ammount Required",
+                })}
                 type={"number"}
-                min="10" 
+                min="10"
                 max="99999"
-              ></input>         
+              ></input>
             </div>
             {errors?.targetPrice && <p>{errors?.targetPrice.message}</p>}
             <div className="mincontribution">
@@ -90,14 +132,18 @@ export const CreateProject = () => {
               <input
                 style={{ fontSize: "1.5rem" }}
                 placeholder="Select Minimum Contribution"
-                {...register("minimumContribution", {required: "*Minimum Contribution Required"})}
+                {...register("minimumContribution", {
+                  required: "*Minimum Contribution Required",
+                })}
                 type={"number"}
-                min="0" 
+                min="0"
                 max="99999"
               ></input>
             </div>
-            {errors?.minimumContribution && <p>{errors?.minimumContribution.message}</p>}
-            <div >
+            {errors?.minimumContribution && (
+              <p>{errors?.minimumContribution.message}</p>
+            )}
+            <div>
               <h2>Upload Image:</h2>
               <input
                 style={{ fontSize: "1.5rem" }}
