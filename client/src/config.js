cf0890beb44325ae5aeabdd631bca40a383702f4
arorashivam <arora.shivam@tftus.com>
2022-11-08 00:58:34 +0530

    add config

---
+++ client/src/config.js
@@ -0,0 +1,4 @@
+export const truffle_development_network = "localhost:9545";
+export const backendPort = "2222";
+export const factoryContractAddress =
+  "0x64BB88cD760B537A7875A860B139680ED6C50176";
